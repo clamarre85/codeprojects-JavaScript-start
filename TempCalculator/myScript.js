4bb0754a6b4ad90adfc46bf23fc3cf7f121056f4
clamarre85 <157823773+clamarre85@users.noreply.github.com>
2024-03-27 13:47:23 -0400

    Add files via upload

---
+++ TempCalculator/myScript.js
@@ -0,0 +1,23 @@
+const celsiusInput = document.getElementById("celsius");
+const fahrenheitInput = document.getElementById("fahrenheit");
+const kelvinInput = document.getElementById("kelvin");
+
+const inputs = document.querySelectorAll(".input");
+
+inputs.forEach(input => {
+    input.addEventListener("input", function () {
+        let value = parseFloat(input.value);
+        console.log(input.name + ":" + value);
+        
+        if (input.name === "celsius") {
+            fahrenheitInput.value = (value * 1.8) + 32;
+            kelvinInput.value = value + 273.15; 
+        } else if (input.name === "fahrenheit") {
+            celsiusInput.value = (value - 32) / 1.8;
+            kelvinInput.value = ((value - 32) /1.8) + 273.15; 
+        } else if (input.name === "kelvin") {
+            fahrenheitInput.value = ((value -273.15) * 1.8) + 32;
+            celsiusInput.value = value - 273.15; 
+        }
+    });
+});
