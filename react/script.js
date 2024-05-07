ba2253897142ecf0e0178551faa986467ee55f17
clamarre85 <157823773+clamarre85@users.noreply.github.com>
2024-05-07 09:43:18 -0400

    Add files via upload

---
+++ react/script.js
@@ -0,0 +1,22 @@
+/*
+function add(a, b){
+return a + b;
+
+}
+*/
+/*
+var result = 60 + add();
+
+add =  (a,b) => {
+return a + b;
+
+}
+console.log(75 + add(20,3));
+document.getElementById("demo").innerHTML = add(6, 3);
+*/
+
+greeting = (name) => "welcome" + name;
+
+document.getElementById("myheading").innerHTML  = greeting("Char")
+
+console.log(greeting("Char"));
